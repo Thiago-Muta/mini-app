24aa88c5cb2b28e3e3148e7b98fd1a9377abfab5
Thiago Muta <thimuta@hotmail.com>
2022-02-17 02:41:14 -0300

    report controller criado

--- app/controllers/reports_controller.rb
+++ app/controllers/reports_controller.rb
@@ -1,4 +1,15 @@
 class ReportsController < ApplicationController
-  
+  before_action :authenticate_user!
+
+  def index
+    @tasks = Task.all
+  end
+
+  # -----complete task only-----
+  def user_reports
+    status = 10
+    type_user = current_user.id
+    @tasks = Task.where(user_id: type_user, status: status)
+  end
 
 end
