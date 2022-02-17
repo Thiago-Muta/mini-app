1ee6621d51b5abe71597e4fa8079ddf56a2d1b8d
Thiago Muta <thimuta@hotmail.com>
2022-02-17 02:20:52 -0300

    spec de criar a task

--- spec/features/user_can_create_task_spec.rb
+++ spec/features/user_can_create_task_spec.rb
@@ -2,18 +2,65 @@ require 'rails_helper'
 
 feature 'User can Create Task' do
   scenario 'Successfully' do
+    user = create(:user)
+    create(:profile, user: user, share: true)
+    task = create(:task, user: user)
+    login_as(user)
+
+    visit new_task_path
+    fill_in 'Title', with: 'New Title'
+    fill_in 'Description', with: 'New Description'
+    click_on 'Create Task'
+
+    expect(Task.last.title).to eq('New Title')
+    expect(Task.last.description).to eq('New Description')
+    expect(page).to have_content('Task Created!')
   end
 
   scenario 'And must be loged in' do
+    visit new_task_path
+    expect(page).to have_content('You need to sign in before continuing.')
   end
 
   scenario 'And Title must have more than 4 characters' do
+    user = create(:user)
+    create(:profile, user: user, share: true)
+    task = create(:task, user: user)
+    login_as(user)
+
+    visit new_task_path
+    fill_in 'Title', with: 'New'
+    fill_in 'Description', with: 'New Description'
+    click_on 'Create Task'
+
+    expect(page).to have_content('Title is short')
   end
 
   scenario 'And Title must have less than 20 characters' do
-  end 
+    user = create(:user)
+    create(:profile, user: user, share: true)
+    task = create(:task, user: user)
+    login_as(user)
 
-  scenario 'And Description Can\'t be blank' do 
+    visit new_task_path
+    fill_in 'Title', with: 'Neeeeeeeeeeeeeeeeeeeeeeew'
+    fill_in 'Description', with: 'New Description'
+    click_on 'Create Task'
+
+    expect(page).to have_content('Title is long')
+  end
+
+  scenario 'And Description Can\'t be blank' do
+    user = create(:user)
+    create(:profile, user: user, share: true)
+    task = create(:task, user: user)
+    login_as(user)
+
+    visit new_task_path
+    fill_in 'Title', with: 'New Title'
+    fill_in 'Description', with: '   '
+    click_on 'Create Task'
+
+    expect(page).to have_content('Description Can\'t be blank')
   end
 end
-
