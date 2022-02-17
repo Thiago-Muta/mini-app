a9ae154d67095982f9e5b2814b0330f0b948427f
Thiago Muta <thimuta@hotmail.com>
2022-02-17 01:56:44 -0300

    spec up

--- spec/features/user_can_change_task_status_spec.rb
+++ spec/features/user_can_change_task_status_spec.rb
@@ -4,6 +4,8 @@ feature 'User can change task status' do
 
   scenario 'On the show page if user is the creator, from incomplete to complete' do
     user = create(:user)
+    profile = create(:profile, user: user)
+    login_as(user)
     task = create(:task, status: 'incomplete', user: user)
 
      visit task_path(task)
@@ -17,6 +19,8 @@ feature 'User can change task status' do
 
   scenario 'On the show page if user is the creator, from complete  to incomplete' do
     user = create(:user)
+    profile = create(:profile, user: user)
+    login_as(user)
     task = create(:task, status: 'complete', user: user)
 
      visit task_path(task)
